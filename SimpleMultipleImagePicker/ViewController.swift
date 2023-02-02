73c8a99e2765cb088c066a73501f0d3a2e64f783
woongs <woongs.rich@kakaocorp.com>
2023-02-02 21:36:48 +0900

    Initial Commit

---
+++ SimpleMultipleImagePicker/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SimpleMultipleImagePicker
+//
+//  Copyright (c) 2023 woongs All rights reserved.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
---
+++ SimpleMultipleImagePickerUITests/SimpleMultipleImagePickerUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  SimpleMultipleImagePickerUITestsLaunchTests.swift
+//  SimpleMultipleImagePicker
+//
+//  Copyright (c) 2023 woongs All rights reserved.
+//
+
+
+import XCTest
+
+final class SimpleMultipleImagePickerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
