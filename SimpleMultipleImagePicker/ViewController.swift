0c0a721c4c0b971ac0f6074a01ad760cecaa30ee
woongs <woongs.rich@kakaocorp.com>
2023-02-03 11:16:47 +0900

    앨범 가져오기

--- SimpleMultipleImagePicker/ViewController.swift
+++ SimpleMultipleImagePicker/ViewController.swift
@@ -7,15 +7,48 @@
 
 
 import UIKit
+import Photos
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate {
+    
+    private var albums: [PHAssetCollection] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
     }
     
-    @IBAction func didPickImagesButtonTap(_ sender: UIButton) {
+    @IBAction
+    private func didPickImagesButtonTap(_ sender: UIButton) {
+        PHPhotoLibrary.requestAuthorization(for: .readWrite) { [weak self] state in
+            guard let self = self else { return }
+            switch state {
+            case .authorized:
+                self.albums = self.loadAlbumsList()
+            default:
+                print(state)
+            }
+        }
+    }
+    
+    private func loadAlbumsList() -> [PHAssetCollection] {
+        var result: [PHAssetCollection] = [PHAssetCollection]()
+        
+        let options = PHFetchOptions()
+        let cameraRoll = PHAssetCollection.fetchAssetCollections(with: .album, subtype: .any, options: options)
+        let favoriteList = PHAssetCollection.fetchAssetCollections(with: .smartAlbum, subtype: .smartAlbumUserLibrary, options: nil)
+        let albumList = PHAssetCollection.fetchAssetCollections(with: .smartAlbum, subtype: .albumRegular, options: nil)
+        
+        [cameraRoll, favoriteList, albumList].forEach {
+            $0.enumerateObjects { collection, _, _ in
+                if !result.contains(collection) {
+                    result.append(collection)
+                }
+            }
+        }
+        
+        return result
     }
 }
 
