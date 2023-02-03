a72c0e5d42fa16898838273b8463f7ceb3807681
woongs <woongs.rich@kakaocorp.com>
2023-02-03 14:43:19 +0900

    앨범 이미지 뿌리기

--- SimpleMultipleImagePicker/ViewController.swift
+++ SimpleMultipleImagePicker/ViewController.swift
@@ -11,12 +11,16 @@ import Photos
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate {
     
+    private var albumImages: [UIImage] = []
     private var albums: [PHAssetCollection] = []
+    
+    @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        self.collectionView.dataSource = self
+        self.collectionView.delegate = self
     }
     
     @IBAction
@@ -25,7 +29,24 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate {
             guard let self = self else { return }
             switch state {
             case .authorized:
-                self.albums = self.loadAlbumsList()
+                let albums = self.loadAlbumsList()
+                self.albums = albums
+                
+                var images: [UIImage] = []
+                albums.forEach { album in
+                    let assets = PHAsset.fetchAssets(in: album, options: nil)
+                    assets.enumerateObjects { asset, _, _ in
+                        PHCachingImageManager.default().requestImage(for: asset, targetSize: PHImageManagerMaximumSize, contentMode: .aspectFit, options: nil) { image, info in
+                            if let image = image {
+                                images.append(image)
+                            }
+                        }
+                    }
+                }
+                self.albumImages = images
+                DispatchQueue.main.async {
+                    self.collectionView.reloadData()
+                }
             default:
                 print(state)
             }
@@ -52,3 +73,29 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate {
     }
 }
 
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.albumImages.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AlbumImageCollectionViewCell", for: indexPath) as? AlbumImageCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        let image = self.albumImages[indexPath.item]
+        
+        cell.configure(image: image)
+        return cell
+    }
+}
+
+class AlbumImageCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var albumImageView: UIImageView!
+    
+    func configure(image: UIImage) {
+        self.albumImageView.image = image
+    }
+}
